2fec2b64142e71aa9bf664b307da06b549b9c673
xueyunfeng <pinus0716@163.com>
2025-01-01 01:15:40 +0800

    :memo: docs(docs): 体操

---
+++ apps/docs/src/knowledge-lib/js/typescript/体操/src/18-获取元组长度/index.ts
@@ -0,0 +1,9 @@
+// 实现：
+type Length<T extends readonly PropertyKey[]> = T['length']
+
+// 用例：
+type tesla = ['tesla', 'model 3', 'model X', 'model Y']
+type spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT']
+
+type teslaLength = Length<tesla> // expected 4
+type spaceXLength = Length<spaceX> // expected 5
